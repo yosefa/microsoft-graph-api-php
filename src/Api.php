e0c8d537330d23f0131957a5c2e59ca2ab266b73
yosefa <yosefaferdianto@outlook.com>
2022-07-18 14:00:26 +0700

    fix Api.php - add exception

--- src/Api.php
+++ src/Api.php
@@ -2,6 +2,7 @@
 namespace MicrosoftGraphApiPhp;
 
 use GuzzleHttp\Exception\ClientException;
+use Microsoft\Graph\Exception\GraphException;
 use Microsoft\Graph\Graph;
 use Microsoft\Graph\Model\User;
 
@@ -19,23 +20,19 @@ class Api
     protected $guzzle;
     protected $graph;
 
-    public function __construct($client_id, $client_secret, $redirect_uri, $tenant_id = "common", $scopes = array("User.Read"), $state = null)
+    public function __construct($client_id, $client_secret, $redirect_uri, $tenant_id = "common", $scopes = array("User.Read"), $state = 42443)
     {
         $this->client_id     = $client_id;
         $this->client_secret = $client_secret;
         $this->redirect_uri  = $redirect_uri;
         $this->tenant_id     = $tenant_id;
         $this->scopes        = $scopes;
+        $this->state         = $state;
 
-        if ($state == null) {
-            $state = 42443;
-        }
-        $this->state = $state;
-
-        $this->guzzle    = new \GuzzleHttp\Client();
         $this->url_token = $this->url_login . $this->tenant_id . "/oauth2/v2.0/token";
 
-        $this->graph = new Graph();
+        $this->guzzle = new \GuzzleHttp\Client();
+        $this->graph  = new Graph();
     }
 
     public function get_url_auth()
@@ -51,34 +48,57 @@ class Api
         return $this->url_login . $this->tenant_id . "/oauth2/v2.0/authorize?" . http_build_query($parameters);
     }
 
-    public function get_token($code)
+    public function get_token($code = null)
     {
-        $request = $this->guzzle->post($this->url_token, array(
-            "form_params" => array(
-                "client_id"     => $this->client_id,
-                "client_secret" => $this->client_secret,
-                "redirect_uri"  => $this->redirect_uri,
-                "scope"         => implode(" ", $this->scopes),
-                "grant_type"    => "authorization_code",
-                "code"          => $code,
-            ),
-        ))->getBody()->getContents();
+        try {
+            $request = $this->guzzle->post($this->url_token, array(
+                "form_params" => array(
+                    "client_id"     => $this->client_id,
+                    "client_secret" => $this->client_secret,
+                    "redirect_uri"  => $this->redirect_uri,
+                    "scope"         => implode(" ", $this->scopes),
+                    "grant_type"    => "authorization_code",
+                    "code"          => $code,
+                ),
+            ))->getBody()->getContents();
 
-        $token = json_decode($request);
-        $this->graph->setAccessToken($token->access_token);
+            $result        = json_decode($request);
+            $result->valid = true;
 
-        return $token;
+            return $result;
+        } catch (ClientException $e) {
+            $response = json_decode($e->getResponse()->getBody()->getContents());
+            $result   = array(
+                'valid'   => false,
+                'message' => isset($response->error_description) ? $response->error_description : 'Error retrieving token.',
+            );
+
+            return (object) $result;
+        } catch (Exception $e) {
+            print_r($e);
+            return false;
+        }
     }
 
     public function get_data($code)
     {
-        $this->get_token($code);
-
-        try {
-            $data = $this->graph->createRequest("get", "/me")->setReturnType(User::class)->execute();
-        } catch (ClientException $e) {
-            print_r($e);
-            return false;
+        $token = $this->get_token($code);
+        $data  = $token;
+        if ($token->valid == true) {
+            try {
+                $this->graph->setAccessToken($token->access_token);
+                $data        = $this->graph->createRequest("get", "/me")->setReturnType(User::class)->execute();
+                $data->valid = true;
+            } catch (ClientException $e) {
+                print_r($e);
+                return false;
+            } catch (GraphException $e) {
+                print_r($e);
+                return false;
+            } catch (Exception $e) {
+                print_r($e);
+                return false;
+            }
         }
 
         return $data;
